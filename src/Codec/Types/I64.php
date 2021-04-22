898568e9776ca8d3ec60862d5591ba2b3b6f6dee
gmajor <gmajorencrypt@gmail.com>
2021-04-22 15:21:58 +0800

    try github action ci

--- src/Codec/Types/I64.php
+++ src/Codec/Types/I64.php
@@ -7,6 +7,7 @@ use BitWasp\Buffertools\ByteOrder;
 use BitWasp\Buffertools\Parser;
 use BitWasp\Buffertools\Types\Int64;
 use Codec\Utils;
+use mysql_xdevapi\Exception;
 
 class I64 extends TInt
 {
@@ -20,13 +21,13 @@ class I64 extends TInt
     function encode ($param)
     {
         $value = strval($param);
-
-        if (gmp_cmp($value, "-9223372036854775808") == 1 && gmp_cmp($value, "9223372036854775808") == -1) {
+        try {
             $i64 = new Int64(ByteOrder::LE);
             $buffer = new Buffer($i64->write($value));
             return Utils::trimHex($buffer->getHex());
+        } catch (\Exception $exception) {
+            throw new \InvalidArgumentException(sprintf('%s range out i64', $value));
         }
-        throw new \InvalidArgumentException(sprintf('%s range out i64', $value));
     }
 
 }
